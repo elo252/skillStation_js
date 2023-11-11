84fdb0f7623f8f44ae71f8127f42c166cd9d8b91
Sahil Kamath <sahilkamath0108@gmail.com>
2023-11-11 13:26:42 +0530

    w4 d1

--- W4_D1.js
+++ W4_D1.js
@@ -1,16 +1,52 @@
-// let greet_one = "Hello"
-// let greet_two = "World!!!"
-// console.log(greet_one)
-// for(let i=0;i<10000000000;i++){
+//Set Timeout
+
+// console.log('Start');
+
+// console.log('delay');
+
+// console.log('End')
+
+
+//with timeout
+
+// console.log('Start');
+
+// setTimeout(function() {
+//   console.log('delay');
+// }, 2000); // Delay for 2 seconds
+
+// console.log('End')
+
+
+//Set interval
+
+// function intervalFunction() {
+//     console.log('Repeated action every 1 second');
+//   }
+  
+// setInterval(intervalFunction, 1000); // Repeats every 1 second
+  
+
+
+
+
+//API
+
+// const api = 'https://official-joke-api.appspot.com/random_joke'
+
+// async function test2(){
+//     const response = await fetch(api);
+//     const data = await response.json();
+//     console.log(data);
 // }
-// console.log(greet_two);
 
+// test2()
 
-//EXPLAINING async here 
-// console.log(greet_one)
-// setTimeout(function(){
-//     console.log("Asynchronous");
-// }, 10000)
-// console.log(greet_two);
+// function test1(){
+//     const response = fetch(api);
+//     const data = response;
+//     console.log(data);
+// }
 
+// test1()
 
